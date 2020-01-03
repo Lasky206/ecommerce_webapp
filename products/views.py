846268c4fd6a4565c0906556cf164225edd6adac
Lasky206 <mistermisterio1000@gmail.com>
2020-01-03 10:29:14 -0800

    Patch

--- products/views.py
+++ products/views.py
@@ -6,8 +6,3 @@ from .models import Product
 class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/list.html"
-
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    #     print(context)
-    #     return context
