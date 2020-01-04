8b9de6dc4d93b4b63bf572defe00129632e17fac
Lasky206 <mistermisterio1000@gmail.com>
2020-01-03 18:41:05 -0800

    Sec 3 Chap 28 Done

--- products/views.py
+++ products/views.py
@@ -7,13 +7,25 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/list.html"
 
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
+
 
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
-        context['abc'] = 123
+        # context['abc'] = 123
         return context
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product doesn't exist")
+        return instance
